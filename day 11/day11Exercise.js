887b09568f17fe4b9dffc81b968428548cadd846
Daniel Lambert <danjlambert95@gmail.com>
2023-02-12 13:20:10 +0100

    destructuring and spreading

---
+++ day 11/day11Exercise.js
@@ -0,0 +1,103 @@
+//Another Example
+const person = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 250,
+  country: 'Finland',
+  job: 'Instructor and Developer',
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB',
+    'Python',
+    'D3.js'
+  ],
+  languages: ['Amharic', 'English', 'Suomi(Finnish)']
+}
+
+// Let us create a function which give information about the person object without destructuring
+
+const getPersonInfo = obj => {
+  const skills = obj.skills
+  const formattedSkills = skills.slice(0, -1).join(', ')
+  const languages = obj.languages
+  const formattedLanguages = languages.slice(0, -1).join(', ')
+
+  let personInfo = `${obj.firstName} ${obj.lastName} lives in ${obj.country}. He is  ${
+    obj.age
+  } years old. He is an ${obj.job}. He teaches ${formattedSkills} and ${
+    skills[skills.length - 1]
+  }. He speaks ${formattedLanguages} and a little bit of ${languages[2]}.`
+
+  return personInfo
+}
+
+const todoList = [
+  {
+    task:'Prepare JS Test',
+    time:'4/1/2020 8:30',
+    completed:true
+  },
+  {
+    task:'Give JS Test',
+    time:'4/1/2020 10:00',
+    completed:false
+  },
+  {
+    task:'Assess Test Result',
+    time:'4/1/2020 1:00',
+    completed:false
+  }
+  ]
+  for (const {task, time, completed} of todoList){
+    console.log(task, time, completed)
+  }
+
+const countries = [
+  'Germany',
+  'France',
+  'Belgium',
+  'Finland',
+  'Sweden',
+  'Norway',
+  'Denmark',
+  'Iceland'
+]
+
+let [gem, fra, , ...nordicCountries] = countries
+
+console.log(gem)
+console.log(fra)
+console.log(nordicCountries)
+
+console.log(getPersonInfo(person))
+
+const evens = [0, 2, 4, 6, 8, 10]
+const evenNumbers = [...evens]
+
+const odds = [1, 3, 5, 7, 9]
+const oddNumbers = [...odds]
+
+const wholeNumbers = [...evens, ...odds]
+
+console.log(evenNumbers)
+console.log(oddNumbers)
+console.log(wholeNumbers)
+
+
+
+
+
+const user = {
+  name:'Asabeneh',
+  title:'Programmer',
+  country:'Finland',
+  city:'Helsinki'
+}
+
+const copiedUser = {...user}
+console.log(copiedUser)
