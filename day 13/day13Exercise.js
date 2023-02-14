875f979f0b738a07b0c0d3445b03caad2d26c37e
Daniel Lambert <danjlambert95@gmail.com>
2023-02-14 19:49:16 +0100

    day13 finished

---
+++ day 13/day13Exercise.js
@@ -0,0 +1,174 @@
+console.log('%c30 Days Of JavaScript', 'color:green') // log output is green
+console.log(
+  '%c30 Days%c %cOf%c %cJavaScript',
+  'color:green',
+  '',
+  'color:red',
+  '',
+  'color:yellow'
+) // log output green red and yellow text
+
+console.warn('This is a warning')
+console.warn(
+  'You are using React. Do not touch the DOM. Virtual DOM will take care of handling the DOM!'
+)
+console.warn('Warning is different from error')
+
+console.error('This is an error message')
+console.error('We all make mistakes')
+
+
+const names = ['Asabeneh', 'Brook', 'David', 'John']
+console.table(names)
+
+const user = {
+    name: 'Asabeneh',
+    title: 'Programmer',
+    country: 'Finland',
+    city: 'Helsinki',
+    age: 250
+  }
+  console.table(user)
+
+
+  const countries = [
+    ['Finland', 'Helsinki'],
+    ['Sweden', 'Stockholm'],
+    ['Norway', 'Oslo']
+  ]
+  console.table(countries)
+
+  const users = [
+    {
+      name: 'Asabeneh',
+      title: 'Programmer',
+      country: 'Finland',
+      city: 'Helsinki',
+      age: 250
+    },
+    {
+      name: 'Eyob',
+      title: 'Teacher',
+      country: 'Sweden',
+      city: 'London',
+      age: 25
+    },
+    {
+      name: 'Asab',
+      title: 'Instructor',
+      country: 'Norway',
+      city: 'Oslo',
+      age: 22
+    },
+    {
+      name: 'Matias',
+      title: 'Developer',
+      country: 'Denmark',
+      city: 'Copenhagen',
+      age: 28
+    }
+  ]
+  console.table(users)
+
+
+//   const countries = [
+//     ['Finland', 'Helsinki'],
+//     ['Sweden', 'Stockholm'],
+//     ['Norway', 'Oslo']
+//   ]
+  
+  console.time('Regular for loop')
+  for (let i = 0; i < countries.length; i++) {
+    console.log(countries[i][0], countries[i][1])
+  }
+  console.timeEnd('Regular for loop')
+  
+  console.time('for of loop')
+  for (const [name, city] of countries) {
+    console.log(name, city)
+  }
+  console.timeEnd('for of loop')
+  
+  console.time('forEach loop')
+  countries.forEach(([name, city]) => {
+    console.log(name, city)
+  })
+  console.timeEnd('forEach loop')
+
+console.info('30 Days Of JavaScript challenge is trending on Github')
+console.info('30 Days Of fullStack challenge might be released')
+console.info('30 Days Of HTML and CSS challenge might be released')
+
+
+console.assert(4 > 3, '4 is greater than 3') // no result
+// console.assert(3 > 4, '3 is not greater than 4') // Assertion failed: 3 is not greater than 4
+
+// for (let i = 0; i <= 10; i += 1) {
+//   let errorMessage = `${i} is not even`
+//   console.log('the # is ' + i)
+//   console.assert(i % 2 === 0, { number: i, errorMessage: errorMessage })
+// }
+
+const namesss = ['Asabeneh', 'Brook', 'David', 'John']
+const countriesss = [
+  ['Finland', 'Helsinki'],
+  ['Sweden', 'Stockholm'],
+  ['Norway', 'Oslo']
+]
+const userss = {
+  name: 'Asabeneh',
+  title: 'Programmer',
+  country: 'Finland',
+  city: 'Helsinki',
+  age: 250
+}
+const usersss = [
+  {
+    name: 'Asabeneh',
+    title: 'Programmer',
+    country: 'Finland',
+    city: 'Helsinki',
+    age: 250
+  },
+  {
+    name: 'Eyob',
+    title: 'Teacher',
+    country: 'Sweden',
+    city: 'London',
+    age: 25
+  },
+  {
+    name: 'Asab',
+    title: 'Instructor',
+    country: 'Norway',
+    city: 'Oslo',
+    age: 22
+  },
+  {
+    name: 'Matias',
+    title: 'Developer',
+    country: 'Denmark',
+    city: 'Copenhagen',
+    age: 28
+  }
+]
+
+console.group('Names')
+console.log(namesss)
+console.groupEnd()
+
+console.group('Countries')
+console.log(countriesss)
+console.groupEnd()
+
+console.group('Users')
+console.log(userss)
+console.log(usersss)
+console.groupEnd()
+
+const func = () => {
+    console.count('Function has been called')
+  }
+  func()
+  func()
+  func()
