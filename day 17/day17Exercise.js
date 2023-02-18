288050686a8a28b9f803801d8a58a7ca04856cbb
Daniel Lambert <danjlambert95@gmail.com>
2023-02-18 12:02:33 +0100

    day17 local and session storage in the browser

---
+++ day 17/day17Exercise.js
@@ -0,0 +1,55 @@
+/*
+Web Storage objects:
+
+localStorage - to display the localStorage object
+localStorage.clear() - to remove everything in the local storage
+localStorage.setItem() - to store data in the localStorage. It takes a key and a value parameters.
+localStorage.getItem() - to display data stored in the localStorage. It takes a key as a parameter.
+localStorage.removeItem() - to remove stored item form a localStorage. It takes key as a parameter.
+localStorage.key() - to display a data stored in a localStorage. It takes index as a parameter.
+
+*/
+
+localStorage.setItem('firstName', 'Daniel') // since the value is string we do not stringify it
+
+localStorage.setItem('age', 200)
+
+
+const skills = ['HTML', 'CSS', 'JS', 'React']
+//Skills array has to be stringified first to keep the format.
+const skillsJSON = JSON.stringify(skills, undefined, 4)
+localStorage.setItem('skills', skillsJSON)
+console.log(localStorage)
+
+let newSkills = [
+  { tech: 'HTML', level: 10 },
+  { tech: 'CSS', level: 9 },
+  { tech: 'JS', level: 8 },
+  { tech: 'React', level: 9 },
+  { tech: 'Redux', level: 10 },
+  { tech: 'Node', level: 8 },
+  { tech: 'MongoDB', level: 8 }
+]
+
+let skillJSON = JSON.stringify(newSkills)
+localStorage.setItem('skills', skillJSON)
+
+const user = {
+  firstName: 'Asabeneh',
+  age: 250,
+  skills: ['HTML', 'CSS', 'JS', 'React']
+}
+
+const userText = JSON.stringify(user, undefined, 4)
+localStorage.setItem('user', userText)
+
+let firstName = localStorage.getItem('firstName')
+let age = localStorage.getItem('age')
+// let skillss = localStorage.getItem('skills')
+console.log(firstName, age)
+
+let skillsss = localStorage.getItem('skills')
+let skillsObj = JSON.parse(skillsss, undefined, 4)
+console.log(skillsObj)
+
+// localStorage.clear()
